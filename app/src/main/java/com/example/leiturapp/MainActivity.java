171a9332a24a0c07ce812b4fec2b6e1de36b8c6d
Augusto Feltrin <108466622+AugustoFeltrin@users.noreply.github.com>
2024-09-02 09:16:35 -0300

    Correcao Fundo + Preludio Pagina

--- app/src/main/java/com/example/leiturapp/MainActivity.java
+++ app/src/main/java/com/example/leiturapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.leiturapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.background), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button button = findViewById(R.id.numero);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Preludio.class);
+                startActivity(intent);
+            }
         });
     }
 }
