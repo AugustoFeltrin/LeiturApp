8083bfa51a9e25478e416cae26ca6a247f2a4cd7
Augusto Feltrin <108466622+AugustoFeltrin@users.noreply.github.com>
2024-09-05 11:27:06 -0300

    Atualizacao Pagina Inicial

--- app/src/main/java/com/example/leiturapp/MainActivity.java
+++ app/src/main/java/com/example/leiturapp/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.leiturapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,7 +28,11 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        Button button = findViewById(R.id.numero);
+        ImageView solImageView = findViewById(R.id.sol);
+        Animation pulseAnimation = AnimationUtils.loadAnimation(this, R.anim.pulse_animation);
+        solImageView.startAnimation(pulseAnimation);
+
+        Button button = findViewById(R.id.iniciar);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
