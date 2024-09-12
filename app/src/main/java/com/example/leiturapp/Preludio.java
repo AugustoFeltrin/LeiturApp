26f274cda48d285e459ad25cdc49e3ccad4c113d
Augusto Feltrin <108466622+AugustoFeltrin@users.noreply.github.com>
2024-09-12 10:13:38 -0300

    Pagina Chamado

--- app/src/main/java/com/example/leiturapp/Preludio.java
+++ app/src/main/java/com/example/leiturapp/Preludio.java
@@ -1,12 +1,26 @@
 package com.example.leiturapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Preludio extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preludio);
+
+        Button btnNext = findViewById(R.id.btn_next);
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Preludio.this, Chamado.class);
+                startActivity(intent);
+            }
+        });
     }
 }
