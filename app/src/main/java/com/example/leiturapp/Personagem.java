fc2b8aac60757a818f3f58c7afe083ce87d5fc9a
Augusto Feltrin <108466622+AugustoFeltrin@users.noreply.github.com>
2024-10-16 17:38:48 -0300

    Atualizacao Paginas - Visual

--- app/src/main/java/com/example/leiturapp/Personagem.java
+++ app/src/main/java/com/example/leiturapp/Personagem.java
@@ -1,7 +1,9 @@
 package com.example.leiturapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,18 +20,33 @@ public class Personagem extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personagem);
 
+        Button btnBack = findViewById(R.id.btnback);
+        Button btnNext = findViewById(R.id.btn_next);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Personagem.this, Revolta.class);
+                startActivity(intent);
+            }
+        });
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Personagem.this, Adaptacoes.class);
+                startActivity(intent);
+            }
+        });
+
         btnMontag = findViewById(R.id.btn_montag);
         layoutDescMontag = findViewById(R.id.layout_desc_montag);
-
         btnClarisse = findViewById(R.id.btn_clarisse);
         layoutDescClarisse = findViewById(R.id.layout_desc_clarisse);
-
         btnMildred = findViewById(R.id.btn_mildred);
         layoutDescMildred = findViewById(R.id.layout_desc_mildred);
-
         btnBeatty = findViewById(R.id.btn_beatty);
         layoutDescBeatty = findViewById(R.id.layout_desc_beatty);
-
         btnFaber = findViewById(R.id.btn_faber);
         layoutDescFaber = findViewById(R.id.layout_desc_faber);
 
@@ -77,3 +94,4 @@ public class Personagem extends AppCompatActivity {
         }
     }
 }
+
